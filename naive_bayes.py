1da0386af2597ac6f8bcbbf7d51017c6f68c19bc
ChelseaRDinh <chelseadinh94@gmail.com>
2018-11-19 14:32:34 -0800

    Added library for computing accuracy score of our classifying model.

--- naive_bayes.py
+++ naive_bayes.py
@@ -2,11 +2,23 @@ import importlib
 import sklearn
 import numpy as np
 import pandas as pd
+#GaussianNB makes most sense because it is rule-based.
 from sklearn.naive_bayes import GaussianNB
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
+
+#Variables for our data set:
+#region
+#designation
+#country
+#province
+#winery
+#variety
+#price(?) --> If we want to deal with numeric attributes.
 
 def main():
-    classifier = MultinomialNB()
+    #Testing sklearn method functionality.
+    classifier = GaussianNB()
     print("Hello world.")
 
 main()
