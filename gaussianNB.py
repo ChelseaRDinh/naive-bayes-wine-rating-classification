bc7c5e3d34d674d41457539c5915a5ea36c49132
ChelseaRDinh <chelseadinh94@gmail.com>
2018-11-19 14:41:33 -0800

    Added first file of training data.

--- gaussianNB.py
+++ gaussianNB.py
@@ -6,6 +6,8 @@ import pandas as pd
 from sklearn.naive_bayes import GaussianNB
 #Calculate accuracy score of our classifier
 from sklearn.metrics import accuracy_score
+#Read train and test data from csv files
+import csv
 
 #Variables for our data set:
 #region
@@ -15,6 +17,9 @@ from sklearn.metrics import accuracy_score
 #winery
 #variety
 #price(?) --> If we want to deal with numeric attributes.
+
+def categorizeData():
+    return 0
 
 def main():
     #Testing sklearn method functionality.
